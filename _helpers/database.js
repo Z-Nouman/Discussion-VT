5a5ff795e73efeaee9eb662bd4147f3acdfc63a4
Zaid Al Nouman <zaidnouman@gmail.com>
2021-01-20 20:38:03 -0500

    Trying something else

--- _helpers/database.js
+++ _helpers/database.js
@@ -1,7 +1,10 @@
 const config = require('../config.json');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
-//    "connectionString": "mongodb://localhost/CS4004",
+// mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+
+const uri = "mongodb+srv://zaid:<password>@discussionvtcluster.j7jsf.mongodb.net/<dbname>?retryWrites=true&w=majority";
+mongoose.connect(uri);
+
 module.exports = {
     User: require('../models/user.model'),
     Course: require('../models/course.model'),
