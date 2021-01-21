33b73a36dca119855826820957897ddc62911ed1
Zaid Al Nouman <zaidnouman@gmail.com>
2021-01-20 20:34:59 -0500

    Figured out some db stuff

--- _helpers/database.js
+++ _helpers/database.js
@@ -1,7 +1,7 @@
 const config = require('../config.json');
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
-
+//    "connectionString": "mongodb://localhost/CS4004",
 module.exports = {
     User: require('../models/user.model'),
     Course: require('../models/course.model'),
