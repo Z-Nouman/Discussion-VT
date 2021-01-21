6350a568d4f80c115663a26b01d38953fc4eba0e
Zaid Al Nouman <zaidnouman@gmail.com>
2021-01-20 21:37:17 -0500

    another

--- _helpers/database.js
+++ _helpers/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
 
 const uri = "mongodb+srv://everyone@discussionvtcluster.j7jsf.mongodb.net/discussionvtcluster?retryWrites=true&w=majority";
-mongoose.connect(uri);
+mongoose.connect(uri, { useCreateIndex: true, useNewUrlParser: true });
 
 module.exports = {
     User: require('../models/user.model'),
