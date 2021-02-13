6f120212984dd8c92e227d61ddb3f82de7b7a183
Zaid Al Nouman <zaidnouman@gmail.com>
2021-02-13 17:01:11 -0500

    Plz work

--- _helpers/database.js
+++ _helpers/database.js
@@ -2,9 +2,10 @@ const config = require('../config.json');
 const mongoose = require('mongoose');
 try{
     mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+    console.log("IT FUCKING WORKED AYYYYYYYYYY")
 
 } catch (e){
-    console.log("THERE IS A FUCKING ERROR YOU DIPSHIT" + e)
+    console.log("THERE IS A FUCKING ERROR YOU DIPSHIT" + e);
 }
 //
 // const uri = "mongodb+srv://zaid:alnouman99@discussionvtcluster.j7jsf.mongodb.net/discussionvtcluster?retryWrites=true&w=majority";
